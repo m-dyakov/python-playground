e2d7087b7ff560b8b933734a419f7a00c8a5dd3b
m-dyakov <michael.dyakov@gmail.com>
2018-07-23 01:18:38 +0200

    Update

--- Simple/main.py
+++ Simple/main.py
@@ -1,1 +1,12 @@
-print('sup')
+import sys
+import methods
+
+if len(sys.argv) == 1:
+    print('No arguments provided')
+    sys.exit(0)
+
+x = sys.argv[1]
+if methods.isInt(x):
+    print('Value {0} is a number'.format(x))
+else:
+    print('Value {0} is not a number'.format(x))
